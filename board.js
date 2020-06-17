575f04b849814a0fc87e961b74f27ab9398566bc
Diogo Ferreira Fernandes <dio@mac.local>
2020-06-17 19:33:59 -0300

    Separate function to update view

--- board.js
+++ board.js
@@ -4,11 +4,15 @@ const board = document.getElementById('board');
 const view = document.getElementById('view');
 board.focus();
 
-// Listener to trigger changes
-board.addEventListener('input', (event) => {
+function updateView() {
   const content = parseContent(board.childNodes);
   view.innerHTML = '';
   content.forEach(node => view.appendChild(node));
+}
+
+// Listener to trigger changes
+board.addEventListener('input', () => {
+  updateView();
 });
 
 
@@ -18,3 +22,36 @@ board.addEventListener('paste', (e) => {
   const text = (e.originalEvent || e).clipboardData.getData('text/plain');
   document.execCommand('insertText', false, text);
 });
+
+// Add text for test porpuses
+const tests = [
+  document.createTextNode('# Comment'),
+  document.createElement('br'),
+  document.createTextNode('variable = 5'),
+  document.createElement('br'),
+  document.createTextNode('variable + 5'),
+  document.createElement('br'),
+  document.createTextNode('variable - 5'),
+  document.createElement('br'),
+  document.createTextNode('variable / 5'),
+  document.createElement('br'),
+  document.createTextNode('variable ^ 5'),
+  document.createElement('br'),
+  document.createTextNode('5 + 5'),
+  document.createElement('br'),
+  document.createTextNode('5 - 5'),
+  document.createElement('br'),
+  document.createTextNode('5 / 5'),
+  document.createElement('br'),
+  document.createTextNode('5 ^ 5'),
+  document.createElement('br'),
+  document.createTextNode('5% of 100'),
+  document.createElement('br'),
+  document.createTextNode('5% on 100'),
+  document.createElement('br'),
+  document.createTextNode('5% off 100')
+];
+
+tests.forEach(test => board.appendChild(test));
+
+updateView();
