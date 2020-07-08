f1319182a9d3772116944176150392547b59c1cd
Diogo Ferreira Fernandes <dio@mac.local>
2020-07-08 08:56:35 -0300

    Update service worker fetch function

--- js/serviceWorker.js
+++ js/serviceWorker.js
@@ -25,10 +25,9 @@ self.addEventListener('install', event => {
   );
 });
 
-self.addEventListener('fetch', fetchEvent => {
-  fetchEvent.respondWith(
-    caches.match(fetchEvent.request).then(res => {
-      return res || fetch(fetchEvent.request)
-    })
-  )
-})
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(res => res || fetch(event.request))
+  );
+});
