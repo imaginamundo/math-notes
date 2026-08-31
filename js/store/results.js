1acf63bbde0b2b37d39d09f15888a16f52235c4a
Diogo Fernandes <diogos.go@gmail.com>
2026-08-31 14:48:46 -0300

    Remove dead code from the results store
    Drop the unused total() helper and the stray delete of a nonexistent
    results.rows property along with the now-unneeded math import.

--- js/store/results.js
+++ js/store/results.js
@@ -1,18 +1,11 @@
-import math from './math.js';
-
 let results = [];
 
 function addResult(value) {
   results.push(value);
 }
 
-function total() {
-  return math.evaluate(results.join(' + '));
-}
-
 function clearResults() {
-  delete results.rows;
   results = [];
 }
 
-export { results, addResult, clearResults, total };
+export { results, addResult, clearResults };
